f461f8d9971c0894ccae9dab34263f5d58c43783
reetuTheCoder <codewithreetu@gmail.com>
2025-10-16 15:29:31 +0530

    check inital up and down  vote value are 0 or not

--- haker_rank_qst/src/Components/CodeReviewFeedback/CodeReviewFeedback.test.js
+++ haker_rank_qst/src/Components/CodeReviewFeedback/CodeReviewFeedback.test.js
@@ -10,7 +10,20 @@ describe("CodeReviewFeedback Component", () => {
 
     aspects.forEach((aspect) => {
       expect(screen.getByText(aspect)).toBeInTheDocument();
-    // expect(screen.queryByText(/goodbye/i)).not.toBeInTheDocument();
+      // expect(screen.queryByText(/goodbye/i)).not.toBeInTheDocument();
+    });
+  });
+
+  test("initial upvotes and downvotes are 0", () => {
+    render(<CodeReviewFeedback />);
+
+    aspects.forEach((ele, idx) => {
+      expect(screen.getAllByText(/Upvotes:/)[idx]).toHaveTextContent(
+        "Upvotes: 0"
+      );
+      expect(screen.getAllByText(/Downvotes:/)[idx]).toHaveTextContent(
+        "Downvotes: 0"
+      );
     });
   });
 });
