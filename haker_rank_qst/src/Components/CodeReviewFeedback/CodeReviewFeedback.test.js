60f76914e528ad02de5039a3a9136adb1d1b1409
reetuTheCoder <codewithreetu@gmail.com>
2025-10-16 16:05:40 +0530

    all test cases are passed

--- haker_rank_qst/src/Components/CodeReviewFeedback/CodeReviewFeedback.test.js
+++ haker_rank_qst/src/Components/CodeReviewFeedback/CodeReviewFeedback.test.js
@@ -18,6 +18,8 @@ describe("CodeReviewFeedback Component", () => {
     render(<CodeReviewFeedback />);
 
     aspects.forEach((ele, idx) => {
+      console.log(ele, "index value", idx);
+
       expect(screen.getAllByText(/Upvotes:/)[idx]).toHaveTextContent(
         "Upvotes: 0"
       );
@@ -25,5 +27,37 @@ describe("CodeReviewFeedback Component", () => {
         "Downvotes: 0"
       );
     });
+  });
+
+  test("clicking upvote increases the count", () => {
+    render(<CodeReviewFeedback />);
+
+    const getUpvodeBtn = screen.getAllByText("👍 Upvote");
+    fireEvent.click(getUpvodeBtn[0]);
+    expect(screen.getAllByText(/Upvotes:/)[0]).toHaveTextContent("Upvotes: 1");
+  });
+
+  test("clicking downvote increases the count", () => {
+    render(<CodeReviewFeedback />);
+
+    const getDownvodeBtn = screen.getAllByText("👎 Downvote");
+    fireEvent.click(getDownvodeBtn[0]);
+    expect(screen.getAllByText(/Downvotes:/)[0]).toHaveTextContent(
+      "Downvotes: 1"
+    );
+  });
+
+  test("animation classes applied on vote and removed after timeout", () => {
+    render(<CodeReviewFeedback />);
+
+    const getUpvodeBtn = screen.getAllByText("👍 Upvote");
+    fireEvent.click(getUpvodeBtn[0]);
+    const getTextValues = screen.getAllByText(/Upvotes:/)[0];
+    console.log("getText", getTextValues);
+    expect(getTextValues).toHaveClass("animate-upvote");
+    jest.advanceTimersByTime(200);
+
+    expect(getTextValues).not.toHaveClass("animate-downvote");
+    // expect(screen.getAllByText(/Upvotes:/)[0]).toHaveTextContent("Upvotes: 1");
   });
 });
